270a481482143936d0f4088a9733cc9c20724bbc
akhilmuvva <you@example.com>
2025-10-31 14:17:07 +0530

    Make ClassifierConfig importable in container (set name to music_genre_classifier) and register via AppConfig path

--- server/music_genre_classifier/apps.py
+++ server/music_genre_classifier/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class ClassifierConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'classifier'
+    # Register the app under the package name so Django can import it inside the container
+    name = 'music_genre_classifier'
